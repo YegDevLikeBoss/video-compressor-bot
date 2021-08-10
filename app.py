6cd3f89e8aad1de847d685a61a645649c3c3ae22
Artyom Yeghoyan <artyomyeghoyan@gmail.com>
2021-08-10 14:24:53 +0300

    feature: added debug mode

--- app.py
+++ app.py
@@ -4,17 +4,18 @@ import telebot
 
 from flask import Flask, request
 
-TOKEN = "1907381812:AAH4Ftxnz1DQlD7JX_KPSoL4MHk-JhBH9zc"
+TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
+DEBUG = bool(os.environ.get('DEBUG'))
 bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+    bot.reply_to(message, "Howdy, how are you doing?")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-	bot.reply_to(message, message.text)
+    bot.reply_to(message, message.text)
 
 @server.route('/' + TOKEN, methods=['POST'])
 def getMessage():
@@ -32,4 +33,8 @@ def webhook():
 
 
 if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    if DEBUG == True:
+        bot.remove_webhook()
+        bot.polling()
+    else:
+        server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
