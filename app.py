a2b3427e70fc349d74bd3f77dd6ea69737f92fb0
Artyom Yeghoyan <artyomyeghoyan@gmail.com>
2021-08-10 15:37:39 +0300

    fix: fixed bug that only text is replyed

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import telebot
 
 from flask import Flask, request
 
+all_content_types_except_video = ['text', 'audio', 'document', 'animation', 'game', 'photo', 'sticker', 'video_note', 'voice', 'contact', 'location', 'venue', 'dice', 'new_chat_members', 'left_chat_member', 'new_chat_title', 'new_chat_photo', 'delete_chat_photo', 'group_chat_created', 'supergroup_chat_created', 'channel_chat_created', 'migrate_to_chat_id', 'migrate_from_chat_id', 'pinned_message', 'invoice', 'successful_payment', 'connected_website', 'poll', 'passport_data', 'proximity_alert_triggered', 'voice_chat_scheduled', 'voice_chat_started', 'voice_chat_ended', 'voice_chat_participants_invited', 'message_auto_delete_timer_changed']
 TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
 DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 bot = telebot.TeleBot(TOKEN)
@@ -13,7 +14,7 @@ server = Flask(__name__)
 def send_welcome(message):
     bot.send_message(message.chat.id, "Отправь мне видео, всё остальное я расскажу потом")
 
-@bot.message_handler(func=lambda message: True)
+@bot.message_handler(content_types=all_content_types_except_video)
 def echo_all(message):
     bot.send_message(message.chat.id, "Моя твоя не понимать, лучше скинь видео")
 
