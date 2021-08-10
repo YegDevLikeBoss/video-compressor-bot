e11a22fd9a10adbced2a23cf051f4aec8226d5ce
Artyom Yeghoyan <artyomyeghoyan@gmail.com>
2021-08-10 14:08:24 +0300

    feature: created webhook

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
+import os
+
 import telebot
 
-bot = telebot.TeleBot("1907381812:AAH4Ftxnz1DQlD7JX_KPSoL4MHk-JhBH9zc")
+from flask import Flask, request
+
+TOKEN = "1907381812:AAH4Ftxnz1DQlD7JX_KPSoL4MHk-JhBH9zc"
+bot = telebot.TeleBot(TOKEN)
+server = Flask(__name__)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
@@ -10,4 +16,20 @@ def send_welcome(message):
 def echo_all(message):
 	bot.reply_to(message, message.text)
 
-bot.polling()
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://video-compressor-bot.herokuapp.com/' + TOKEN)
+    return "!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
