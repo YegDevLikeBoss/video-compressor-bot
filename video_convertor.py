be7918f09cdb2675fa7d90ff76d263261ea8bf8d
Artyom Yeghoyan <artyomyeghoyan@gmail.com>
2021-08-11 13:38:01 +0300

    made video note conversion

--- video_convertor.py
+++ video_convertor.py
@@ -1,8 +1,27 @@
 from enum import Enum
+from moviepy.editor import VideoFileClip
 
 class ConversionType(Enum):
     SMALL_SIZE = 'small_size'
     NOTE_SIZE = 'note_size'
 
-def convert_video(file_data, conversion_type):
-    return file_data
+def convert_video(file_id, conversion_type):
+    conversion_type = ConversionType(conversion_type)
+    if conversion_type == ConversionType.NOTE_SIZE:
+        convert_to_video_note(file_id)
+    elif conversion_type == ConversionType.SMALL_SIZE:
+        compress_video(file_id)
+
+def convert_to_video_note(file_id):
+    clip = VideoFileClip(f'videos/{file_id}.mp4')
+
+    if clip.duration >= 60:
+        clip = clip.subclip(0,59)
+
+    clip = clip.resize(height=640)
+    clip = clip.crop(x_center=clip.size[0]//2, width=640)
+
+    clip.write_videofile(f"videos/converted/{file_id}.mp4")
+
+def compress_video(file_id):
+    pass
